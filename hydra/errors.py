51f6349c1cc2c18c9832d9d6d42b188e1f8389f4
Omry Yadan <omry@yadan.net>
2026-09-18 08:12:28 +0200

    Document InstantiationException class

--- hydra/errors.py
+++ hydra/errors.py
@@ -15,7 +15,8 @@ class OverrideParseException(CompactHydraException):
         self.message = message
 
 
-class InstantiationException(HydraException): ...
+class InstantiationException(HydraException):
+    """An error raised by Hydra during instantiation."""
 
 
 class ConfigCompositionException(CompactHydraException): ...
